e2e5abf1d69b8709212302edaf5627e8fa271f09
LeaKySky <59387168+LeaKySky@users.noreply.github.com>
2021-02-07 22:45:47 +0530

    Add files via upload
    This code attempts to alphebatize an array in ascending order based on peoples name eg: given a series of names it will re-order it with everyone's names from a-z.

---
+++ alphabetize.java
@@ -0,0 +1,12 @@
+public class alphabetize
+{
+	public static void main(String[] args)
+	{
+		//System.out.println("Hello World");
+		String[] array = {"Sasha", "Rishi", "Rohan", "Arisya"};
+		for(int i = 0; i < array.length; i++)
+		{
+			System.out.println(array[i]);
+		}	
+	}
+}
